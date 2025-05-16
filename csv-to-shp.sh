b9bdb2cfa11344467bb60277bf1fc337e563d07f
Antruc <admin@antruc.dev>
2025-05-16 16:46:28 -0700

    Update csv-to-shp.sh

--- csv-to-shp.sh
+++ csv-to-shp.sh
@@ -2,27 +2,20 @@
 
 # Convert csv files to shape using qgis_process
 
-if [ -n "$1" ]
-then
-   cd "$(realpath $1)"
-for file in *; do
-
-    if [ -f "$file" ]; then
-
-        if [[ $file == *.csv ]]; then
-
-  	    if grep -q ° "$file"; then
-  	        dms="yes"
-            else
-                dms="no"
-	    fi
-
-            qgis_process run native:concavehull --no-python --distance_units=meters --area_units=m2 --ellipsoid=EPSG:7030 --INPUT=delimitedtext:"//file://"$(realpath "$file")"?type=csv&maxFields=10000&detectTypes=yes&xyDms="$dms"&xField=x&yField=y&crs=EPSG:4326&spatialIndex=no&subsetIndex=no&watchFile=no" --ALPHA=0.3 --HOLES=true --NO_MULTIGEOMETRY=false --OUTPUT=""$(realpath "${file%.*}.shp")""
-         
-        fi
-    fi
-done
-
+if [ -n "$1" ] then
+	cd "$(realpath $1)"
+	for file in *; do
+    	if [ -f "$file" ]; then
+        	if [[ $file == *.csv ]]; then
+  	    		if grep -q ° "$file"; then
+  	        		dms="yes"
+            	else
+                	dms="no"
+	    		fi
+            	qgis_process run native:concavehull --no-python --distance_units=meters --area_units=m2 --ellipsoid=EPSG:7030 --INPUT=delimitedtext:"//file://"$(realpath "$file")"?type=csv&maxFields=10000&detectTypes=yes&xyDms="$dms"&xField=x&yField=y&crs=EPSG:4326&spatialIndex=no&subsetIndex=no&watchFile=no" --ALPHA=0.3 --HOLES=true --NO_MULTIGEOMETRY=false --OUTPUT=""$(realpath "${file%.*}.shp")""
+        	fi
+    	fi
+	done
 else
    echo "Usage: ./csv-to-shp.sh dirname"
 fi
