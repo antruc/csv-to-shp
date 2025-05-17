12093b84e00f59b266fece7efdb6d598d7fff9f2
Antruc <admin@antruc.dev>
2025-05-16 17:03:51 -0700

    Improve error handling

--- csv-to-shp.sh
+++ csv-to-shp.sh
@@ -2,10 +2,10 @@
 
 # Convert csv files to shape using qgis_process
 
-if [ -n "$1" ] then
+if [[ -n "$1" ]] then
 	cd "$(realpath $1)"
 	for file in *; do
-    	if [ -f "$file" ]; then
+		if [ -f "$file" ]; then
         	if [[ $file == *.csv ]]; then
   	    		if grep -q ° "$file"; then
   	        		dms="yes"
@@ -14,7 +14,9 @@ if [ -n "$1" ] then
 	    		fi
             	qgis_process run native:concavehull --no-python --distance_units=meters --area_units=m2 --ellipsoid=EPSG:7030 --INPUT=delimitedtext:"//file://"$(realpath "$file")"?type=csv&maxFields=10000&detectTypes=yes&xyDms="$dms"&xField=x&yField=y&crs=EPSG:4326&spatialIndex=no&subsetIndex=no&watchFile=no" --ALPHA=0.3 --HOLES=true --NO_MULTIGEOMETRY=false --OUTPUT=""$(realpath "${file%.*}.shp")""
         	fi
-    	fi
+    	else
+			echo "Error: Empty folder"
+		fi
 	done
 else
    echo "Usage: ./csv-to-shp.sh dirname"
